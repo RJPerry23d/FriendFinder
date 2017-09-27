53e902291ffea47a23db13e4d2800d0acd837c67
RJ <rj@blacklambs.com>
2017-09-27 00:21:15 -0400

    updated Survey and Home html

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//Friends that are pre loaded
+
+var friendsArray = [{
+    name: "Superman",
+    photo: "https://vignette.wikia.nocookie.net/superman/images/b/b1/Superman_Action_976_Gary_Frank.png/revision/latest/scale-to-width-down/288?cb=20170501140424",
+    answers: ["1","2","3","4","5","4","3","2","1","2"]
+},
+    
+{
+    name: "Batman",
+    photo: "https://upload.wikimedia.org/wikipedia/en/1/17/Batman-BenAffleck.jpg",
+    answers: ["5","2","3","4","5","4","3","2","1","2"]
+},
+{
+    name: "The Flash",
+    photo: "https://images.moviepilot.com/image/upload/c_fill,h_470,q_auto:good,w_620/cdhgjbdxcefp2gvygkr2.jpg",
+    answers: ["3","3","5","4","1","4","3","5","2","5"]
+}
+    
+]
+//exports the friends array to be used by other files
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,14 @@
+var path = require("path");
+var friendsArray = require ('../data/friends.js');
+
+module.exports = function (app) {
+    app.get('/api/friends', function (req, res){
+        res.json(friendsArray);
+    });
+
+      app.post("/api/friends", function(req, res) {    
+    friendsArray.push(req.body);
+      res.json(true);
+  });
+
+};
